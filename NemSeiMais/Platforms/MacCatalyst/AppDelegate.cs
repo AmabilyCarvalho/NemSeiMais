712d42a9d72666a66177d65f81344e07bf73163d
amabily <amabilymangolin123@gmail.com>
2024-10-31 10:50:20 -0300

    .

---
+++ NemSeiMais/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace NemSeiMais;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
